aa30dab32b6d0bf821ccf9ea2b57c4ceba6e65c4
Mark Haviland <mhaviland@cpaglobal.com>
2019-04-26 16:59:17 -0500

    updates...

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,21 @@
       <v-container fill-height>
         <v-layout align-space-around justify-center fill-height column>
           <v-card dark color="primary" flat height="50%">
-          <div class="text-xs-center">
-            <v-btn fab dark large color="purple">
-              <v-icon>add</v-icon>
-            </v-btn>
-            <div class="power">up</div>
-          </div>
-         </v-card>
-         <v-card dark color="secondary" flat height="50%">
-          <div class="text-xs-center">
-            <v-btn fab dark large color="blue">
-              <v-icon>add</v-icon>
-            </v-btn>
-            <div class="power">down</div>
-          </div>
-        </v-card>
+            <div class="text-xs-center">
+              <v-btn fab dark large color="purple">
+                <v-icon>add</v-icon>
+              </v-btn>
+              <div class="power">up</div>
+            </div>
+          </v-card>
+          <v-card dark color="secondary" flat height="50%">
+            <div class="text-xs-center">
+              <div class="power">down</div>
+              <v-btn bottom fab dark large color="blue">
+                <v-icon>add</v-icon>
+              </v-btn>
+            </div>
+          </v-card>
         </v-layout>
       </v-container>
     </v-content>
@@ -34,9 +34,9 @@ import AppZones from '@/components/AppZones';
 export default {
   name: 'App',
   components: {
-      AppZones
+    AppZones
   }
-}
+};
 </script>
 <style scoped>
 .content-container {
@@ -44,8 +44,8 @@ export default {
   height: 50vh;
 }
 .power {
-  position: absolute;
+  /* position: absolute;
   top: 50%;
-  left: 50%;
+  left: 50%; */
 }
 </style>
--- src/components/AppZones.vue
+++ src/components/AppZones.vue
@@ -1,20 +1,19 @@
 <template>
   <div>this is zones view</div>
-
 </template>
 
 <script>
-  export default {
-    name: 'AppZones',
-    data() {
-      return {
-        appTitle: 'VSX 1021-k',
-        selectedZone: 'main',
-        zones: {
-          main: {},
-          zoneOne: {}
-        }
+export default {
+  name: 'AppZones',
+  data() {
+    return {
+      appTitle: 'VSX 1021-k',
+      selectedZone: 'main',
+      zones: {
+        main: {},
+        zoneOne: {}
       }
-    }
+    };
   }
+};
 </script>
